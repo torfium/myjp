17a9f3a00baf564342fcba07970fcb995f5faa30
torfium <staskrasnuhin@gmail.com>
2023-10-26 21:05:31 +0300

    ура победа

---
+++ View/Pages/Page201.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AAAAAAAAAAAA_SLOMALI.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Window201.xaml
+    /// </summary>
+    public partial class Page201 : Page
+    {
+        public Page201()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAnswer_Click(object sender, RoutedEventArgs e)
+        {
+            //1
+            try
+            {
+                MessageBox.Show($"G = {(Math.Exp(2 * Convert.ToDouble(TbY.Text)) + Math.Sin(Convert.ToDouble(TbF.Text))) / (Math.Log(3.8 * Convert.ToDouble(TbY.Text) + Convert.ToDouble(TbF.Text)))}");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Неправильно! Попробуй ещё раз!");
+            }
+            
+            
+        }
+    }
+}
---
+++ View/Pages/Task307Page.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AAAAAAAAAAAA_SLOMALI.View.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Task307Page.xaml
+    /// </summary>
+    public partial class Task307Page : Page
+    {
+        public Task307Page()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAnswerA_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show($"Значение y для x ({Convert.ToDouble(TbA.Text)}) = {2 * Math.Tan(Convert.ToDouble(TbA.Text))}");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Функция не определена для значения х");
+            }
+        }
+
+        private void BtnAnswerB_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                MessageBox.Show($"Значение y для x ({Convert.ToDouble(TbB.Text)}) = {3 / (Convert.ToDouble(TbB.Text) - 1)}");
+            }
+            catch (Exception)
+            {
+                MessageBox.Show($"Функция не определена для значения х");
+            }
+        }
+    }
+}
